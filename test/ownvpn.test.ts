ed347dac3987bf582f749d9cf8332f75d9f298ac
Mika Majakorpi <mika.majakorpi@nitor.com>
2025-07-17 13:09:49 +0530

    Initial commit

---
+++ test/ownvpn.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Ownvpn from '../lib/ownvpn-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/ownvpn-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Ownvpn.OwnvpnStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
