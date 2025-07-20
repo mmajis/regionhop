89d47846b0b35bc493c7c2338ab16f4f290a97f4
Mika Majakorpi <mika.majakorpi@nitor.com>
2025-07-20 18:45:15 +0530

    remove old default config echo

--- lib/ownvpn-compute-stack.ts
+++ lib/ownvpn-compute-stack.ts
@@ -220,7 +220,6 @@ export class OwnvpnComputeStack extends cdk.Stack {
       'echo "Backing up newly created scripts in case they changed..."',
       `aws s3 sync /etc/wireguard s3://${s3BucketName}/wireguard-config/ --exclude "*.tmp" --region ${targetRegion}`,
       'echo "WireGuard VPN server setup completed!"',
-      'echo "Client configuration available at: /etc/wireguard/clients/macos-client/"',
     );
 
     // Create Launch Template for spot instances
